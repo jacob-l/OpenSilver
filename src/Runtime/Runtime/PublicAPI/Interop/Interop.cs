6d2d3a2bfb97f7955c360feeb78eef1827497bdd
Iakov Lilo <jlilo@hwdtech.com>
2022-01-17 10:21:42 +0600

    SL should fail

--- src/Runtime/Runtime/PublicAPI/Interop/Interop.cs
+++ src/Runtime/Runtime/PublicAPI/Interop/Interop.cs
@@ -82,6 +82,9 @@ namespace OpenSilver
             return CSHTML5.INTERNAL_InteropImplementation.ExecuteJavaScript_SimulatorImplementation(javascript, runAsynchronously: true, variables: variables);
         }
 
+#if MIGRATION
+asdasd
+#endif
         /// <summary>
         /// Unboxes the value passed as a parameter. It is particularly useful for the variables of the ExecuteJavaScript Methods calls aimed at using third party libraries.
         /// </summary>
