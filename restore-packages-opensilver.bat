b09f9a7bcce23d4a25be275f5cdebb5cba959f27
Iakov Lilo <iakov.lilo@userware.onmicrosoft.com>
2023-03-03 10:16:58 +1100

    chore: send nul to bypass pause at the end of script

--- restore-packages-opensilver.bat
+++ restore-packages-opensilver.bat
@@ -1,3 +1,2 @@
 build\nuget.exe install OpenSilver -Version 1.1.0 -OutputDirectory src\packages -Source https://api.nuget.org/v3/index.json
 build\nuget.exe install OpenSilver.UwpCompatible -Version 1.1.0 -OutputDirectory src\packages -Source https://api.nuget.org/v3/index.json
-pause
