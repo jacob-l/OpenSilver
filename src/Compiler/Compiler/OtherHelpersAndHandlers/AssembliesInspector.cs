7d777a93bed49ea469c2de0c025504fc1fd302ee
Iakov Lilo <iakov.lilo@userware.onmicrosoft.com>
2023-03-13 12:22:28 +1100

    Migrate to mono.cecil

--- src/Compiler/Compiler/OtherHelpersAndHandlers/AssembliesInspector.cs
+++ src/Compiler/Compiler/OtherHelpersAndHandlers/AssembliesInspector.cs
@@ -1,4 +1,5 @@
 
+
 /*===================================================================================
 * 
 *   Copyright (c) Userware (OpenSilver.net, CSHTML5.com)
@@ -13,102 +14,119 @@
 \*====================================================================================*/
 
 using System;
+using System.Collections.Generic;
 using System.Reflection;
 using System.Xml.Linq;
-using OpenSilver.Compiler.Common;
+using OpenSilver.Compiler.OtherHelpersAndHandlers.MonoCecilAssembliesInspector;
 
 namespace OpenSilver.Compiler
 {
-    public sealed class ReflectionOnSeparateAppDomainHandler : ReflectionOnSeparateAppDomainHandlerBase<IMarshalledObject, MarshalledObject>
+    public class AssembliesInspector : IDisposable
     {
-        //Note: we use a new AppDomain so that we can Unload all the assemblies that we have inspected when we have done.
 
         //----------------------------------------------------------------------
         // We create a static instance in the "BeforeXamlPreprocessor" task.
         // The static instance avoids reloading the assemblies for each XAML file.
         // We dispose it in the "AfterXamlPreprocessor" task.
         //----------------------------------------------------------------------
+        public static AssembliesInspector Current;
 
-        public static ReflectionOnSeparateAppDomainHandler Current;
+        private readonly MonoCecilAssembliesInspectorImpl _monoCecilVersion;
 
-        public ReflectionOnSeparateAppDomainHandler(bool isSLMigration)
-            : base(isSLMigration)
+        public AssembliesInspector(bool isSlMigration)
         {
+            _monoCecilVersion = new MonoCecilAssembliesInspectorImpl(isSlMigration);
+        }
+
+        public void Dispose()
+        {
+            // Unload everything:
+            _monoCecilVersion.Dispose();
+            GC.Collect(); // Collects all unused memory
+            GC.WaitForPendingFinalizers(); // Waits until GC has finished its work
+            GC.Collect();
+        }
+
+        public void LoadAssembly(string assemblyPath, bool loadReferencedAssembliesToo, bool skipReadingAttributesFromAssemblies)
+        {
+            _monoCecilVersion.LoadAssembly(assemblyPath, loadReferencedAssembliesToo, skipReadingAttributesFromAssemblies);
         }
 
         public string GetContentPropertyName(string namespaceName, string localTypeName, string assemblyNameIfAny = null)
-            => MarshalledObject.GetContentPropertyName(namespaceName, localTypeName, assemblyNameIfAny);
+            => _monoCecilVersion.GetContentPropertyName(namespaceName, localTypeName, assemblyNameIfAny);
 
         public bool IsPropertyAttached(string propertyName, string declaringTypeNamespaceName, string declaringTypeLocalName, string parentNamespaceName, string parentLocalTypeName, string declaringTypeAssemblyIfAny = null)
-            => MarshalledObject.IsPropertyAttached(propertyName, declaringTypeNamespaceName, declaringTypeLocalName, parentNamespaceName, parentLocalTypeName, declaringTypeAssemblyIfAny);
+            => _monoCecilVersion.IsPropertyAttached(propertyName, declaringTypeNamespaceName, declaringTypeLocalName, parentNamespaceName, parentLocalTypeName, declaringTypeAssemblyIfAny);
 
         public bool IsPropertyOrFieldACollection(string propertyName, string parentNamespaceName, string parentLocalTypeName, string parentAssemblyNameIfAny = null)
-            => MarshalledObject.IsPropertyOrFieldACollection(propertyName, parentNamespaceName, parentLocalTypeName, parentAssemblyNameIfAny);
+            => _monoCecilVersion.IsPropertyOrFieldACollection(propertyName, parentNamespaceName, parentLocalTypeName, parentAssemblyNameIfAny);
 
         public bool IsPropertyOrFieldADictionary(string propertyName, string parentNamespaceName, string parentLocalTypeName, string parentAssemblyNameIfAny = null)
-            => MarshalledObject.IsPropertyOrFieldADictionary(propertyName, parentNamespaceName, parentLocalTypeName, parentAssemblyNameIfAny);
+            => _monoCecilVersion.IsPropertyOrFieldADictionary(propertyName, parentNamespaceName, parentLocalTypeName, parentAssemblyNameIfAny);
 
         public bool DoesMethodReturnACollection(string methodName, string typeNamespaceName, string localTypeName, string typeAssemblyNameIfAny = null)
-            => MarshalledObject.DoesMethodReturnACollection(methodName, typeNamespaceName, localTypeName, typeAssemblyNameIfAny);
+            => _monoCecilVersion.DoesMethodReturnACollection(methodName, typeNamespaceName, localTypeName, typeAssemblyNameIfAny);
 
         public bool DoesMethodReturnADictionary(string methodName, string typeNamespaceName, string localTypeName, string typeAssemblyNameIfAny = null)
-            => MarshalledObject.DoesMethodReturnADictionary(methodName, typeNamespaceName, localTypeName, typeAssemblyNameIfAny);
+            => _monoCecilVersion.DoesMethodReturnADictionary(methodName, typeNamespaceName, localTypeName, typeAssemblyNameIfAny);
 
         public bool IsElementADictionary(string parentNamespaceName, string parentLocalTypeName, string parentAssemblyNameIfAny = null)
-            => MarshalledObject.IsElementADictionary(parentNamespaceName, parentLocalTypeName, parentAssemblyNameIfAny);
+            => _monoCecilVersion.IsElementADictionary(parentNamespaceName, parentLocalTypeName, parentAssemblyNameIfAny);
 
         public bool IsElementAMarkupExtension(string parentNamespaceName, string parentLocalTypeName, string parentAssemblyNameIfAny = null)
-            => MarshalledObject.IsElementAMarkupExtension(parentNamespaceName, parentLocalTypeName, parentAssemblyNameIfAny);
+            => _monoCecilVersion.IsElementAMarkupExtension(parentNamespaceName, parentLocalTypeName, parentAssemblyNameIfAny);
 
         public bool IsTypeAssignableFrom(string nameSpaceOfTypeToAssignFrom, string nameOfTypeToAssignFrom, string assemblyNameOfTypeToAssignFrom, string nameSpaceOfTypeToAssignTo, string nameOfTypeToAssignTo, string assemblyNameOfTypeToAssignTo, bool isAttached = false)
-            => MarshalledObject.IsTypeAssignableFrom(nameSpaceOfTypeToAssignFrom, nameOfTypeToAssignFrom, assemblyNameOfTypeToAssignFrom, nameSpaceOfTypeToAssignTo, nameOfTypeToAssignTo, assemblyNameOfTypeToAssignTo, isAttached);
+            => _monoCecilVersion.IsTypeAssignableFrom(nameSpaceOfTypeToAssignFrom, nameOfTypeToAssignFrom, assemblyNameOfTypeToAssignFrom, nameSpaceOfTypeToAssignTo, nameOfTypeToAssignTo, assemblyNameOfTypeToAssignTo, isAttached);
 
         public string GetKeyNameOfProperty(string elementNameSpace, string elementLocalName, string assemblyNameIfAny, string propertyName)
-            => MarshalledObject.GetKeyNameOfProperty(elementNameSpace, elementLocalName, assemblyNameIfAny, propertyName);
+            => _monoCecilVersion.GetKeyNameOfProperty(elementNameSpace, elementLocalName, assemblyNameIfAny, propertyName);
 
         public bool DoesTypeContainNameMemberOfTypeString(string namespaceName, string localTypeName, string assemblyNameIfAny = null)
-            => MarshalledObject.DoesTypeContainNameMemberOfTypeString(namespaceName, localTypeName, assemblyNameIfAny);
+            => _monoCecilVersion.DoesTypeContainNameMemberOfTypeString(namespaceName, localTypeName, assemblyNameIfAny);
 
-        public XName GetCSharpEquivalentOfXamlTypeAsXName(string namespaceName, string localTypeName, string assemblyNameIfAny = null, bool ifTypeNotFoundTryGuessing = false)
-            => MarshalledObject.GetCSharpEquivalentOfXamlTypeAsXName(namespaceName, localTypeName, assemblyNameIfAny, ifTypeNotFoundTryGuessing);
-
-        public Type GetCSharpEquivalentOfXamlType(string namespaceName, string localTypeName, string assemblyIfAny = null, bool ifTypeNotFoundTryGuessing = false)
-            => MarshalledObject.GetCSharpEquivalentOfXamlType(namespaceName, localTypeName, assemblyIfAny, ifTypeNotFoundTryGuessing);
+        public XName GetCSharpEquivalentOfXamlTypeAsXName(string namespaceName, string localTypeName, string assemblyNameIfAny = null)
+            => _monoCecilVersion.GetCSharpEquivalentOfXamlTypeAsXName(namespaceName, localTypeName, assemblyNameIfAny);
 
         public string GetCSharpEquivalentOfXamlTypeAsString(string namespaceName, string localTypeName, string assemblyNameIfAny = null, bool ifTypeNotFoundTryGuessing = false)
-            => MarshalledObject.GetCSharpEquivalentOfXamlTypeAsString(namespaceName, localTypeName, assemblyNameIfAny, ifTypeNotFoundTryGuessing);
+            => _monoCecilVersion.GetCSharpEquivalentOfXamlTypeAsString(namespaceName, localTypeName,
+                assemblyNameIfAny, ifTypeNotFoundTryGuessing);
 
         public string GetAssemblyQualifiedNameOfXamlType(string namespaceName, string localTypeName, string assemblyName)
-            => MarshalledObject.GetAssemblyQualifiedNameOfXamlType(namespaceName, localTypeName, assemblyName);
+            => _monoCecilVersion.GetAssemblyQualifiedNameOfXamlType(namespaceName, localTypeName, assemblyName);
 
         public MemberTypes GetMemberType(string memberName, string namespaceName, string localTypeName, string assemblyNameIfAny = null)
-            => MarshalledObject.GetMemberType(memberName, namespaceName, localTypeName, assemblyNameIfAny);
+            => _monoCecilVersion.GetMemberType(memberName, namespaceName, localTypeName, assemblyNameIfAny);
 
         public bool IsTypeAnEnum(string namespaceName, string localTypeName, string assemblyNameIfAny = null)
-            => MarshalledObject.IsTypeAnEnum(namespaceName, localTypeName, assemblyNameIfAny);
+            => _monoCecilVersion.IsTypeAnEnum(namespaceName, localTypeName, assemblyNameIfAny);
 
         public void GetMethodReturnValueTypeInfo(string methodName, string namespaceName, string localTypeName, out string returnValueNamespaceName, out string returnValueLocalTypeName, out string returnValueAssemblyName, out bool isTypeString, out bool isTypeEnum, string assemblyNameIfAny = null)
-            => MarshalledObject.GetMethodReturnValueTypeInfo(methodName, namespaceName, localTypeName, out returnValueNamespaceName, out returnValueLocalTypeName, out returnValueAssemblyName, out isTypeString, out isTypeEnum, assemblyNameIfAny);
+            => _monoCecilVersion.GetMethodReturnValueTypeInfo(methodName, namespaceName, localTypeName, out returnValueNamespaceName, out returnValueLocalTypeName, out returnValueAssemblyName, out isTypeString, out isTypeEnum, assemblyNameIfAny);
 
         public void GetAttachedPropertyGetMethodInfo(string methodName, string namespaceName, string localTypeName, out string declaringTypeName, out string returnValueNamespaceName, out string returnValueLocalTypeName, out bool isTypeString, out bool isTypeEnum, string assemblyNameIfAny = null)
-            => MarshalledObject.GetAttachedPropertyGetMethodInfo(methodName, namespaceName, localTypeName, out declaringTypeName, out returnValueNamespaceName, out returnValueLocalTypeName, out isTypeString, out isTypeEnum, assemblyNameIfAny);
+            => _monoCecilVersion.GetAttachedPropertyGetMethodInfo(methodName, namespaceName, localTypeName,
+                out declaringTypeName, out returnValueNamespaceName, out returnValueLocalTypeName, out isTypeString,
+                out isTypeEnum, assemblyNameIfAny);
 
         public void GetPropertyOrFieldTypeInfo(string propertyOrFieldName, string namespaceName, string localTypeName, out string propertyNamespaceName, out string propertyLocalTypeName, out string propertyAssemblyName, out bool isTypeString, out bool isTypeEnum, string assemblyNameIfAny = null, bool isAttached = false)
-            => MarshalledObject.GetPropertyOrFieldTypeInfo(propertyOrFieldName, namespaceName, localTypeName, out propertyNamespaceName, out propertyLocalTypeName, out propertyAssemblyName, out isTypeString, out isTypeEnum, assemblyNameIfAny, isAttached: isAttached);
+            => _monoCecilVersion.GetPropertyOrFieldTypeInfo(propertyOrFieldName, namespaceName, localTypeName,
+                out propertyNamespaceName, out propertyLocalTypeName, out propertyAssemblyName, out isTypeString,
+                out isTypeEnum, assemblyNameIfAny, isAttached: isAttached);
 
         public void GetPropertyOrFieldInfo(string propertyOrFieldName, string namespaceName, string localTypeName, out string memberDeclaringTypeName, out string memberTypeNamespace, out string memberTypeName, out bool isTypeString, out bool isTypeEnum, string assemblyNameIfAny = null, bool isAttached = false)
-            => MarshalledObject.GetPropertyOrFieldInfo(propertyOrFieldName, namespaceName, localTypeName, out memberDeclaringTypeName, out memberTypeNamespace, out memberTypeName, out isTypeString, out isTypeEnum, assemblyNameIfAny, isAttached);
+            => _monoCecilVersion.GetPropertyOrFieldInfo(propertyOrFieldName, namespaceName, localTypeName, out memberDeclaringTypeName, out memberTypeNamespace, out memberTypeName, out isTypeString, out isTypeEnum, assemblyNameIfAny, isAttached);
 
         public string GetFieldName(string fieldNameIgnoreCase, string namespaceName, string localTypeName, string assemblyIfAny = null)
-            => MarshalledObject.GetFieldName(fieldNameIgnoreCase, namespaceName, localTypeName, assemblyIfAny);
+            => _monoCecilVersion.GetFieldName(fieldNameIgnoreCase, namespaceName, localTypeName, assemblyIfAny);
 
         public bool IsAssignableFrom(string namespaceName, string typeName, string fromNamespaceName, string fromTypeName)
-            => MarshalledObject.IsAssignableFrom(namespaceName, typeName, fromNamespaceName, fromTypeName);
+            => _monoCecilVersion.IsAssignableFrom(namespaceName, typeName, fromNamespaceName, fromTypeName);
 
         public string GetField(string fieldName, string namespaceName, string typeName, string assemblyName)
-            => MarshalledObject.GetField(fieldName, namespaceName, typeName, assemblyName);
+            => _monoCecilVersion.GetField(fieldName, namespaceName, typeName, assemblyName);
 
         public string GetEventHandlerType(string eventName, string namespaceName, string typeName, string assemblyName)
-            => MarshalledObject.GetEventHandlerType(eventName, namespaceName, typeName, assemblyName);
+            => _monoCecilVersion.GetEventHandlerType(eventName, namespaceName, typeName, assemblyName);
     }
 }
---
+++ src/Compiler/Compiler/OtherHelpersAndHandlers/MonoCecilAssembliesInspector/TypeReferenceExtensions.cs
@@ -0,0 +1,73 @@
+﻿
+/*===================================================================================
+*
+*   Copyright (c) Userware/OpenSilver.net
+*
+*   This file is part of the OpenSilver Runtime (https://opensilver.net), which is
+*   licensed under the MIT license: https://opensource.org/licenses/MIT
+*
+*   As stated in the MIT license, "the above copyright notice and this permission
+*   notice shall be included in all copies or substantial portions of the Software."
+*
+\*====================================================================================*/
+
+using Mono.Cecil;
+using Mono.Cecil.Rocks;
+using System;
+using System.Linq;
+
+namespace OpenSilver.Compiler.OtherHelpersAndHandlers.MonoCecilAssembliesInspector
+{
+    internal static class TypeReferenceExtensions
+    {
+        public static TypeReference ResolveGenericParameter(this TypeReference typeRef, TypeReference elementType)
+        {
+            var genericParameter = typeRef as GenericParameter;
+            if (genericParameter == null)
+            {
+                throw new ArgumentException("Type Reference must be a GenericParameter");
+            }
+
+            var genericType = genericParameter.DeclaringType.GetGenericInstanceType(elementType);
+
+            return genericType.GenericArguments[genericParameter.Position];
+        }
+
+        public static GenericInstanceType GetGenericInstanceType(this TypeReference typeRef, TypeReference elementType)
+        {
+            elementType = elementType.Resolve()?.BaseType;
+            var typeDef = typeRef.Resolve();
+            while (elementType != null && typeDef != elementType.Resolve())
+            {
+                elementType = elementType.Resolve()?.BaseType;
+            }
+
+            return elementType as GenericInstanceType;
+        }
+
+        public static TypeReference PopulateGeneric(this TypeReference typeRef, TypeReference rootElementType, TypeReference ownerElementType)
+        {
+            if (typeRef.IsGenericParameter)
+            {
+                return typeRef.ResolveGenericParameter(rootElementType);
+            }
+
+            if (typeRef is GenericInstanceType instance)
+            {
+                var cc = ownerElementType.GetGenericInstanceType(rootElementType);
+                var arg = instance.GenericArguments.Select(ga =>
+                {
+                    if (ga is GenericParameter genericParameter)
+                    {
+                        return cc.GenericArguments[genericParameter.Position];
+                    }
+
+                    return ga.PopulateGeneric(rootElementType, ownerElementType);
+                }).ToArray();
+                return instance.ElementType.MakeGenericInstanceType(arg);
+            }
+
+            return typeRef;
+        }
+    }
+}
