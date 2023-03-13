e6a274acf1c2146969e917f2bd6c4ef683e07827
Iakov Lilo <iakov.lilo@userware.onmicrosoft.com>
2023-03-13 12:22:28 +1100

    Migrate to mono.cecil

--- src/Compiler/Compiler/EntryPoints/XamlPreprocessor.cs
+++ src/Compiler/Compiler/EntryPoints/XamlPreprocessor.cs
@@ -140,7 +140,7 @@ namespace OpenSilver.Compiler
                         if (shouldTheFileBeProcessed)
                         {
                             // The "ReflectionOnSeparateAppDomainHandler" class lets us use a separate AppDomain to resolve the types so that the types can be unloaded when done (when disposed, it frees any hook on the user application DLL's):
-                            ReflectionOnSeparateAppDomainHandler reflectionOnSeparateAppDomain = ReflectionOnSeparateAppDomainHandler.Current; // Note: this is not supposed to be null because it was instantiated in the "BeforeXamlPreprocessor" task. We use a static instance to avoid reloading the assemblies for each XAML file that is processed.
+                            AssembliesInspector reflectionOnSeparateAppDomain = AssembliesInspector.Current; // Note: this is not supposed to be null because it was instantiated in the "BeforeXamlPreprocessor" task. We use a static instance to avoid reloading the assemblies for each XAML file that is processed.
 
                             // Make sure that the reference is not null:
                             if (reflectionOnSeparateAppDomain == null)
@@ -188,7 +188,7 @@ namespace OpenSilver.Compiler
                     issues when the user recompiles his application). So we free them now.
                  */
 
-                ReflectionOnSeparateAppDomainHandler.Current.Dispose(); // Note: this is not supposed to be null because it was instantiated in the "BeforeXamlPreprocessor" task.
+                AssembliesInspector.Current.Dispose(); // Note: this is not supposed to be null because it was instantiated in the "BeforeXamlPreprocessor" task.
 
                 //-----------------------------------------------------
                 // Display the error and cancel the Build process:
---
+++ src/Compiler/Compiler/OtherHelpersAndHandlers/MonoCecilAssembliesInspector/MemberReferenceExtensions.cs
@@ -0,0 +1,27 @@
+﻿
+/*===================================================================================
+*
+*   Copyright (c) Userware/OpenSilver.net
+*
+*   This file is part of the OpenSilver Runtime (https://opensilver.net), which is
+*   licensed under the MIT license: https://opensource.org/licenses/MIT
+*
+*   As stated in the MIT license, "the above copyright notice and this permission
+*   notice shall be included in all copies or substantial portions of the Software."
+*
+\*====================================================================================*/
+
+using Mono.Cecil;
+
+namespace OpenSilver.Compiler.OtherHelpersAndHandlers.MonoCecilAssembliesInspector
+{
+    internal static class MemberReferenceExtensions
+    {
+        private const string SystemString = "System.String";
+
+        public static bool IsString(this MemberReference type)
+        {
+            return type.FullName == SystemString;
+        }
+    }
+}
